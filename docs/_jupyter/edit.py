8dff4c52957101a4e23647d8456943dd8671fa74
anilg <62422148+anilgeorge04@users.noreply.github.com>
2020-11-25 14:58:45 +0530

    automate jupyter to markdown

---
+++ docs/_jupyter/edit.py
@@ -0,0 +1,17 @@
+# Reference: https://jaketae.github.io/blog/jupyter-automation/
+#!/usr/bin/python
+import sys, re
+
+def edit():
+	path = "/home/anilg/projects/newblog/docs/_jupyter/" + str(sys.argv[1])
+	yaml = "---\ntitle: TITLE\nmathjax: true\ncategories:\n  - category\ntags:\n  - tag\n---\n\n"
+	with open(path, 'r') as file:
+		filedata = file.read()
+	filedata = re.sub(r"!\[png\]\(", "<img src=\"/assets/images/", filedata)
+	filedata = re.sub(".png\)", ".png\">", filedata)
+	filedata = yaml + filedata
+	with open(path, 'w') as file:
+		file.write(filedata)
+
+if __name__ == '__main__':
+	edit()
