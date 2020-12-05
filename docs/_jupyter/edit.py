c7b678750c496f318d13847d5f967f0433e3b4fe
anilg <62422148+anilgeorge04@users.noreply.github.com>
2020-12-05 14:27:10 +0530

    update edit.py
    - update link to blog folder (works)
    - updated convert script to match
    - add notebook line (to test)

--- docs/_jupyter/edit.py
+++ docs/_jupyter/edit.py
@@ -3,13 +3,14 @@
 import sys, re
 
 def edit():
-	path = "/home/anilg/projects/newblog/docs/_jupyter/" + str(sys.argv[1])
-	yaml = "---\ntitle: TITLE\nexcerpt: Shortexcerpt\nheader:\n  overlay_image: HEADERIMAGE\n  overlay_filter: 0.5\n  caption: CAPTION\nnbviewer: bitlylink\nmathjax: true\ntags:\n  - tag\n---\n\n"
+	path = "/home/anilg/blog/docs/_jupyter/" + str(sys.argv[1])
+	yaml = "---\ntitle: TITLE\nexcerpt: Shortexcerpt\nheader:\n  teaser: TEASERIMAGE\n  overlay_image: HEADERIMAGE\n  overlay_filter: 0.5\n  caption: CAPTION\nnbviewer: bitlylink\nmathjax: true\ntags:\n  - tag\n---\n\n"
+	notebook = "\n{% include /notebooks/nbviewer.html %}\n"
 	with open(path, 'r') as file:
 		filedata = file.read()
 	filedata = re.sub(r"!\[png\]\(", "<img src=\"/assets/images/", filedata)
 	filedata = re.sub(".png\)", ".png\">", filedata)
-	filedata = yaml + filedata
+	filedata = yaml + filedata + notebook
 	with open(path, 'w') as file:
 		file.write(filedata)
 
