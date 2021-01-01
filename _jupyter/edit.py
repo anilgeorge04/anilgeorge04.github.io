3cc53137ce72e8c60617143bd3cf8f833c446b20
anilg <62422148+anilgeorge04@users.noreply.github.com>
2021-01-01 18:22:08 +0530

    update edit.py path remove docs

--- _jupyter/edit.py
+++ _jupyter/edit.py
@@ -3,7 +3,7 @@
 import sys, re
 
 def edit():
-	path = "/home/anilg/blog/docs/_jupyter/" + str(sys.argv[1])
+	path = "/home/anilg/blog/_jupyter/" + str(sys.argv[1])
 	yaml = "---\ntitle: TITLE\nexcerpt: Shortexcerpt\nheader:\n  teaser: TEASERIMAGE\n  overlay_image: HEADERIMAGE\n  overlay_filter: 0.5\n  caption: CAPTION\nnbviewer: bitlylink\nmathjax: true\ntags:\n  - tag\n---\n\n"
 	notebook = "\n{% include /notebooks/nbviewer.html %}\n"
 	with open(path, 'r') as file:
